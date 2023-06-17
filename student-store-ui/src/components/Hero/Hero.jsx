92f572a472a2aa77fca89361f19e779cc6c44998
Anaya Bussey <anayabussey@gmail.com>
2023-06-16 20:30:50 -0400

    w/ video

---
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+    return (
+        <div id="intro">
+          <div className="words">
+            <h1>Welcome! <br/>Find Your Goodies!</h1>
+            <p>We have all kinds of merch. 
+            Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
+            </div>
+          <div className="store">
+            <img src="store.gif" />
+          </div>
+  
+        </div>
+    )
+  }
--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -5,23 +5,12 @@ import { categories } from '../../constants'
 import CategoryView from "../CategoryView/CategoryView";
 import { useState } from "react";
 import ProductView from "../ProductView/ProductView"
+import Hero from "../Hero/Hero"
 
 export default function Home() {
   return (
     <div className="home">
-      <div id="intro">
-        <div className="words">
-          <h1>Welcome! <br/>Find Your Goodies!</h1>
-          <p>We have all kinds of merch. 
-          Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
-          </div>
-        <div className="store">
-          <img src="store.gif" />
-        </div>
-
-      </div>
-      
-      
+      <Hero/>
     </div>
   )
 }
